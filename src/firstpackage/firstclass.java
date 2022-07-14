31bc5b2c112b37dc72850ec1fb6b7e7ee5780c02
dmccormick <dmccormick@192.168.132.18>
2022-07-14 12:59:55 -0400

    2nd commit for real real

--- src/firstpackage/firstclass.java
+++ src/firstpackage/firstclass.java
@@ -2,6 +2,6 @@ package firstpackage;
 
 public class firstclass {
 	public static void main(String[] args) {
-		System.out.println("We will be starting over again");
+		System.out.println("We will doing it over again");
 	}
 }	
