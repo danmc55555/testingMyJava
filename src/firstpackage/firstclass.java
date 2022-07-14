4872988739ba77939d8bfd0153a896e7fb8f3ff5
dmccormick <dmccormick@192.168.132.18>
2022-07-14 12:58:45 -0400

    2nd commit

--- src/firstpackage/firstclass.java
+++ src/firstpackage/firstclass.java
@@ -2,6 +2,6 @@ package firstpackage;
 
 public class firstclass {
 	public static void main(String[] args) {
-		System.out.println("We are starting over");
+		System.out.println("We are starting over again");
 	}
 }	
