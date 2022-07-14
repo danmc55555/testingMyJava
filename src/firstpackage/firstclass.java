377e17ac8993f821910b053b27719acf962504c8
dmccormick <dmccormick@192.168.132.18>
2022-07-14 11:11:15 -0400

    checking a git pull

--- src/firstpackage/firstclass.java
+++ src/firstpackage/firstclass.java
@@ -4,6 +4,6 @@ public class firstclass {
 	public static void main(String[] args) {
 		System.out.println("Hello Bro");
 		System.out.println("I changed this");
-		System.out.println("trying to make a new branch");
+		System.out.println("deleted the branch");
 	}
 }	
