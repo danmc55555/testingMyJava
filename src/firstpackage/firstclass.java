dce6794d2efccb8af5988d799842d2b42e3143b0
dmccormick <dmccormick@192.168.132.18>
2022-07-14 12:54:25 -0400

    "1st commit on starting over branch"

--- src/firstpackage/firstclass.java
+++ src/firstpackage/firstclass.java
@@ -2,10 +2,6 @@ package firstpackage;
 
 public class firstclass {
 	public static void main(String[] args) {
-		System.out.println("Hello Bro");
-		System.out.println("I changed this");
-		System.out.println("trying to make a new branch");
-		System.out.println("extra shit");
-		System.out.println("deleted the branch");
+		System.out.println("We are starting over");
 	}
 }	
