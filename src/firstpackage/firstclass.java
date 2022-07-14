4e6c7f392af366c0fecf2b3547c1b5ce3323ac3b
danmc55555 <52798377+danmc55555@users.noreply.github.com>
2022-07-14 13:02:34 -0400

    Update firstclass.java

--- src/firstpackage/firstclass.java
+++ src/firstpackage/firstclass.java
@@ -3,5 +3,6 @@ package firstpackage;
 public class firstclass {
 	public static void main(String[] args) {
 		System.out.println("We are starting over");
+		System.out.println("Yeah we are");
 	}
 }	
